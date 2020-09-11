2ed426544e0f2dd407738a27571c11b92177fc71
Cameron <cameron.242325@nv.ccsd.net>
2020-09-10 18:52:53 -0700

    Fix math error

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Rolls a random number from 0-100',
     return: false,
     run: async (client, message) => {
-        message.channel.send(Math.floor(Math.random(101)));
+        message.channel.send(Math.floor(Math.random() * 101));
     },
 };
