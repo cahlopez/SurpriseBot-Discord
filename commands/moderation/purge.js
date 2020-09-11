3855ba36e6db48f99567fc072ce809e15c70c55b
Cameron <cameron.242325@nv.ccsd.net>
2020-09-11 12:54:16 -0700

    Remove accounting for extra message

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -13,7 +13,7 @@ module.exports = {
         }
 
         // Get the delete count, as an actual number.
-        const deleteCount = parseInt(args[0], 10) + 1;
+        const deleteCount = parseInt(args[0], 10);
 
         // Set a min and max for deleteCount
         if(!deleteCount || deleteCount < 2 || deleteCount > 100) {
