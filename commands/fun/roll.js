47c0e69191a57563082342ab22c43373fcf96aba
Cameron <cameron.242325@nv.ccsd.net>
2020-09-12 18:37:15 -0700

    Update both whitelists

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -2,9 +2,7 @@ module.exports = {
     name: 'roll',
     description: 'Rolls a random number from 0-100',
     run: async (client, message, args) => {
-        const whitelist = ['176538779010596864', 272865436847308800];
-        console.log(message.author.id);
-        console.log(whitelist[0]);
+        const whitelist = ['176538779010596864', '272865436847308800'];
 
         if(whitelist.indexOf(message.author.id) != -1 && args[0] != null) {
             message.channel.send(message.author.username + ' rolled: ' + args[0]);
