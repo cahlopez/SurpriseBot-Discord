3bec0e99d587e359c04f570e688d90e1aeb668a8
Cameron <cameron.242325@nv.ccsd.net>
2022-02-01 11:38:39 -0800

    Update autoReaction ID

--- dist/listeners/autoReaction.js
+++ dist/listeners/autoReaction.js
@@ -4,8 +4,7 @@ exports.default = (client) => {
     client.on("messageCreate", async (message) => {
         console.log(message.author.username);
         console.log(message.author.id);
-        if (message.author.id == "937913319996682291") {
-            console.log("CORRECT USER");
+        if (message.author.id == "176538779010596864") {
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
             await message.react('👎');
         }
--- src/listeners/autoReaction.ts
+++ src/listeners/autoReaction.ts
@@ -5,8 +5,7 @@ export default (client: Client): void => {
     client.on("messageCreate", async (message: Message) => {
         console.log(message.author.username);
         console.log(message.author.id);
-        if(message.author.id == "937913319996682291") {
-            console.log("CORRECT USER")
+        if(message.author.id == "176538779010596864") {
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
             await message.react('👎');
         }
