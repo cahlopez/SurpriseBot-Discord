8f5b7d01d57f115c29ba249e8284ad2249bb4123
Cameron <cameron.242325@nv.ccsd.net>
2022-02-01 11:41:17 -0800

    Update autoReaction.ts

--- src/listeners/autoReaction.ts
+++ src/listeners/autoReaction.ts
@@ -3,9 +3,7 @@ import { Commands } from "../commands";
 
 export default (client: Client): void => {
     client.on("messageCreate", async (message: Message) => {
-        console.log(message.author.username);
-        console.log(message.author.id);
-        if(message.author.id == "176538779010596864") {
+        if(message.author.id == "244305794512846858") {
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
             await message.react('👎');
         }
