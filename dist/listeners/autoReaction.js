9c7551384beeed65b39cde78ca583e621b3fa340
Cameron <cameron.242325@nv.ccsd.net>
2022-02-01 12:01:39 -0800

    Re-compile autoReaction

--- dist/listeners/autoReaction.js
+++ dist/listeners/autoReaction.js
@@ -2,9 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (client) => {
     client.on("messageCreate", async (message) => {
-        console.log(message.author.username);
-        console.log(message.author.id);
-        if (message.author.id == "176538779010596864") {
+        if (message.author.id == "244305794512846858") {
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
             await message.react('👎');
         }
