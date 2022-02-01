976a172de997af8b75fddb60d7ad8a9d9db625dd
Cameron <cameron.242325@nv.ccsd.net>
2022-02-01 11:09:24 -0800

    Add prints to auto reactions

--- dist/listeners/autoReaction.js
+++ dist/listeners/autoReaction.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (client) => {
     client.on("message", async (message) => {
+        console.log("NEW MESSAGE");
         if (message.author.id == "937913319996682291") {
+            console.log("CORRECT USER");
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
             await message.react('👎');
         }
--- src/listeners/autoReaction.ts
+++ src/listeners/autoReaction.ts
@@ -3,7 +3,9 @@ import { Commands } from "../commands";
 
 export default (client: Client): void => {
     client.on("message", async (message: Message) => {
+        console.log("NEW MESSAGE")
         if(message.author.id == "937913319996682291") {
+            console.log("CORRECT USER")
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
             await message.react('👎');
         }
