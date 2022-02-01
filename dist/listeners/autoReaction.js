6aff5f37803fe7f72a9f440dbb5657a4f7bc53f0
Cameron <cameron.242325@nv.ccsd.net>
2022-02-01 11:36:08 -0800

    Update autoReaction prints

--- dist/listeners/autoReaction.js
+++ dist/listeners/autoReaction.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (client) => {
     client.on("messageCreate", async (message) => {
-        console.log("NEW MESSAGE");
+        console.log(message.author.username);
+        console.log(message.author.id);
         if (message.author.id == "937913319996682291") {
             console.log("CORRECT USER");
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
--- src/listeners/autoReaction.ts
+++ src/listeners/autoReaction.ts
@@ -3,7 +3,8 @@ import { Commands } from "../commands";
 
 export default (client: Client): void => {
     client.on("messageCreate", async (message: Message) => {
-        console.log("NEW MESSAGE")
+        console.log(message.author.username);
+        console.log(message.author.id);
         if(message.author.id == "937913319996682291") {
             console.log("CORRECT USER")
             //message.react(message.guild!.emojis.cache.get('21') as EmojiIdentifierResolvable);
