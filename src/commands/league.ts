2664b3cd8491de983cb47f33ecf3d087b92fc29a
Cameron <cameronaleclopez@gmail.com>
2022-07-14 01:30:03 -0700

    Add recent ranked games to league command and add try/catch

--- src/commands/league.ts
+++ src/commands/league.ts
@@ -1,7 +1,9 @@
 import { ApplicationCommandOptionTypes, ApplicationCommandTypes, Embed, DiscordEmbedField, Emoji, Collection} from "../../deps.ts";
+import { log } from "../utils/logger.ts"
 import { reply } from "../utils/reply.ts";
 import { createCommand } from "./mod.ts";
 import { configs } from "../../configs.ts";
+import { Bot as fBot} from "../../bot.ts";
 
 const rankedEmojis: Record<string, bigint> = {
   "IRON": 996645679873134593n,
@@ -28,88 +30,140 @@ createCommand({
     }
   ],
   execute: async (Bot, interaction) => {
-    const sumData = await APIFetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${interaction.data!.options![0].value}`);
+    try {
+      const sumData = await APIFetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${interaction.data!.options![0].value}`);
 
-    if(sumData.status) return reply(Bot, interaction, { content: "I was unable to find a Summoner by that name!" });
+      if(sumData.status) return reply(Bot, interaction, { content: "I was unable to find a Summoner by that name!" });
 
-    // deno-lint-ignore no-explicit-any
-    const leagueData: any = await APIFetch(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${sumData.id}`);
+      const leagueData = await APIFetch(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${sumData.id}`);
+      const champData = await APIFetch(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${sumData.id}`);
+      const allChamps = await APIFetch(`http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/championFull.json`);
+      const recMatch = await APIFetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${sumData.puuid}/ids?type=ranked&start=0&count=3`)
 
-    
-    
-    // deno-lint-ignore no-explicit-any
-    const champData: any = await APIFetch(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${sumData.id}`);
-    // deno-lint-ignore no-explicit-any
-    const allChamps: any = await APIFetch(`http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/championFull.json`);
+      let fieldRank: DiscordEmbedField = {name: "Rank", value: `*Unranked*`, inline: true};
+      const recChampsField: DiscordEmbedField = {name: "Recent Ranked Games", value: `\u17b5 \u17b5 \u17b5 \u17b5 \u17b5 \u17b5 \u17b5 *No games found*`, inline: true};
 
-    let fieldRank: DiscordEmbedField = {name: "Level", value: `${sumData.summonerLevel}`, inline: true};
+      if(leagueData) {
+        for(const key in leagueData) {
+          if(leagueData[key].queueType == 'RANKED_SOLO_5x5') {
+            const tierEmoji = rankedEmojis[leagueData[key].tier];
 
-    if(leagueData) {
-      for(const key in leagueData) {
-        if(leagueData[key].queueType == 'RANKED_SOLO_5x5') {
-          const tierEmoji = rankedEmojis[leagueData[key].tier];
-
-          fieldRank = {name: "Rank", value: `<:${(leagueData[key].tier).toLowerCase()}:${tierEmoji}> ${leagueData[key].tier} ${leagueData[key].rank}`, inline: true};
+            fieldRank = {name: "Rank", value: `<:${(leagueData[key].tier).toLowerCase()}:${tierEmoji}> ${leagueData[key].tier} ${leagueData[key].rank}`, inline: true};
+          }
         }
       }
-    }
 
-    const topChamps: Array<number> = [];
-    const champPoints: Array<string | number> = [];
-    const champIcons: Array<Emoji> = [];
+      const topChamps: Array<string> = [];
+      const recChamps: Array<string> = [];
+      const champPoints: Array<string | number> = [];
+      const topChampIcons: Array<Emoji> = [];
+      const recChampIcons: Array<Emoji> = [];
+      // deno-lint-ignore no-explicit-any
+      const recMatchData: any[][] = []
 
-    for(let i = 0; i < 3; i++) {
-      topChamps.push(allChamps.keys[champData[i].championId]);
-      champPoints.push(nFormatter(champData[i].championPoints));
+      for(const id in recMatch) {
+        const matchInfo = await APIFetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${recMatch[id]}`);
 
-      const champEmoji = (await Bot.helpers.getEmojis(975295750126846013n)).find(emoji => emoji.name === topChamps[i].toString());
-      if(!champEmoji) {
-        const allEmojis: Collection<bigint, Emoji> = await Bot.helpers.getEmojis(975295750126846013n);
+        for(const par in matchInfo.info.participants) {
+          if(matchInfo.info.participants[par].puuid == sumData.puuid) {
+            const tempArr = [];
 
-        if(allEmojis.size > 47) {
-          allEmojis.forEach(emoji => Bot.helpers.deleteEmoji(975295750126846013n, emoji.id!))
-        } else {
-          champIcons.push(await Bot.helpers.createEmoji(975295750126846013n, { name: topChamps[i].toString(), image: `http://ddragon.leagueoflegends.com/cdn/12.13.1/img/champion/${topChamps[i]}.png`}))
+            tempArr.push(matchInfo.info.participants[par].championName);
+
+            if(matchInfo.info.participants[par].win) {
+              tempArr.push("W");
+            } else {
+              tempArr.push("L");
+            }
+
+            tempArr.push(matchInfo.info.participants[par].kills);
+            tempArr.push(matchInfo.info.participants[par].deaths);
+            tempArr.push(matchInfo.info.participants[par].assists);
+
+            recMatchData.push(tempArr);
+          }
         }
+      }
+
+      for(let i = 0; i < 3; i++) {
+        topChamps.push(allChamps.keys[champData[i].championId]);
+        champPoints.push(nFormatter(champData[i].championPoints));
+
+        await doesEmojiExist(Bot, topChamps[i], topChampIcons);
+      }
+
+      if(recMatchData.length > 0) {
+        recChampsField.value = "";
+
+        for(let i = 0; i < recMatchData.length; i++) {
+          recChamps.push(recMatchData[i][0]);
+
+          await doesEmojiExist(Bot, recChamps[i], recChampIcons);
+          recChampsField.value += `<:${recChampIcons[i].name}:${recChampIcons[i].id}> ${recChamps[0]} - **${recMatchData[i][2]}/${recMatchData[i][3]}/${recMatchData[i][4]} (${recMatchData[i][1]})** \n`
+        }
+      }
+
+      const date = new Date();
+      let message: Embed;
+
+      //((interaction.data!.options![0].value)!.toString()).toLowerCase() == "tomaterman"
+      if(false) {
+        message = Bot.transformers.embed(Bot, { 
+          title: "League of Legends Stats", 
+          timestamp: `${("0" + date.getDate()).slice(-2)}/${("0" + (date.getMonth() + 1)).slice(-2)}/${date.getFullYear()}`,
+          thumbnail: { url: `http://ddragon.leagueoflegends.com/cdn/12.13.1/img/profileicon/${sumData.profileIconId}.png`},
+          fields: [
+            {name: "Player", value: `${sumData.name}`, inline: true},
+            {name: "Level", value: `${sumData.summonerLevel}`, inline: true},
+            {name: "Rank", value: `<:iron:996645679873134593> Iron IV`, inline: true}, 
+            {name: '\u200B', value: '\u200B'},
+            {
+              name: "Top Champions", 
+              value: `<:${topChampIcons[0].name}:${topChampIcons[0].id}> ${topChamps[0]} - **0**
+              <:${topChampIcons[1].name}:${topChampIcons[1].id}> ${topChamps[1]} - **0**
+              <:${topChampIcons[2].name}:${topChampIcons[2].id}> ${topChamps[2]} - **0**`, 
+              inline: true
+            },
+            {
+              name: "Recent Ranked Games", 
+              value: `<:${topChampIcons[2].name}:${topChampIcons[2].id}> ${topChamps[0]} - **0/20/0 (L)** 
+              <:${topChampIcons[0].name}:${topChampIcons[0].id}> ${topChamps[0]} - **0/20/0 (L)** 
+              <:${topChampIcons[1].name}:${topChampIcons[1].id}> ${topChamps[0]} - **0/20/0 (L)**`, 
+              inline: true
+            },
+          ]
+        });
       } else {
-        champIcons.push(champEmoji);
+        message = Bot.transformers.embed(Bot, { 
+          title: "League of Legends Stats", 
+          timestamp: `${("0" + date.getDate()).slice(-2)}/${("0" + (date.getMonth() + 1)).slice(-2)}/${date.getFullYear()}`,
+          thumbnail: { url: `http://ddragon.leagueoflegends.com/cdn/12.13.1/img/profileicon/${sumData.profileIconId}.png`},
+          fields: [
+            {name: "Player", value: `${sumData.name}`, inline: true},
+            {name: "Level", value: `${sumData.summonerLevel}`, inline: true},
+            fieldRank,
+            {
+              name: "Top Champions", 
+              value: `<:${topChampIcons[0].name}:${topChampIcons[0].id}> ${topChamps[0]} - **${champPoints[0]}**
+              <:${topChampIcons[1].name}:${topChampIcons[1].id}> ${topChamps[1]} - **${champPoints[1]}**
+              <:${topChampIcons[2].name}:${topChampIcons[2].id}> ${topChamps[2]} - **${champPoints[2]}**`,
+              inline: true
+            },
+            recChampsField,
+          ]
+        });
       }
+
+      await reply(Bot, interaction, { embeds: [message] });
+    } catch (error) {
+      log.error(error);
+
+      await reply(Bot, interaction, { content: "An unknown error occured, please try again!" });
     }
-
-    const date = new Date();
-    let message: Embed;
-
-    if(((interaction.data!.options![0].value)!.toString()).toLowerCase() == "tomaterman") {
-      message = Bot.transformers.embed(Bot, { 
-        title: "League of Legends Stats", 
-        timestamp: `${("0" + date.getDate()).slice(-2)}/${("0" + (date.getMonth() + 1)).slice(-2)}/${date.getFullYear()}`,
-        thumbnail: { url: `http://ddragon.leagueoflegends.com/cdn/12.13.1/img/profileicon/${sumData.profileIconId}.png`},
-        fields: [
-          {name: "Player", value: `${sumData.name}`, inline: true},
-          {name: "Rank", value: `<:iron:996645679873134593> Iron IV`, inline: true}, 
-          {name: '\u200B', value: '\u200B'},
-          {name: "Top Champions", value: `<:${champIcons[0].name}:${champIcons[0].id}> ${topChamps[0]} - **0** \n <:${champIcons[1].name}:${champIcons[1].id}> ${topChamps[1]} - **0** \n <:${champIcons[2].name}:${champIcons[2].id}> ${topChamps[2]} - **0**`}
-        ]
-      });
-    } else {
-      message = Bot.transformers.embed(Bot, { 
-        title: "League of Legends Stats", 
-        timestamp: `${("0" + date.getDate()).slice(-2)}/${("0" + (date.getMonth() + 1)).slice(-2)}/${date.getFullYear()}`,
-        thumbnail: { url: `http://ddragon.leagueoflegends.com/cdn/12.13.1/img/profileicon/${sumData.profileIconId}.png`},
-        fields: [
-          {name: "Player", value: `${sumData.name}`, inline: true},
-          fieldRank, 
-          {name: '\u200B', value: '\u200B'},
-          {name: "Top Champions", value: `<:${champIcons[0].name}:${champIcons[0].id}> ${topChamps[0]} - **${champPoints[0]}** \n <:${champIcons[1].name}:${champIcons[1].id}> ${topChamps[1]} - **${champPoints[1]}** \n <:${champIcons[2].name}:${champIcons[2].id}> ${topChamps[2]} - **${champPoints[2]}**`}
-        ]
-      });
-    }
-
-    await reply(Bot, interaction, { embeds: [message] });
   },
 });
 
-async function APIFetch(url: string): Promise<Record<string, unknown>> {
+async function APIFetch(url: string) {
     const response = await fetch(url, {
         method: "GET",
         headers: {
@@ -119,6 +173,25 @@ async function APIFetch(url: string): Promise<Record<string, unknown>> {
 
     //return JSON.parse(new TextDecoder().decode((await response.body?.getReader().read()!).value));
     return response.json();
+}
+
+async function doesEmojiExist(bot: typeof fBot, wantedEmoji: string, arr: Array<Emoji>) {
+  // console.log(wantedEmoji);
+  const champEmoji = (await bot.helpers.getEmojis(975295750126846013n)).find(emoji => emoji.name === wantedEmoji);
+
+  if(!champEmoji) {
+    const allEmojis: Collection<bigint, Emoji> = await bot.helpers.getEmojis(975295750126846013n);
+
+    if(allEmojis.size > 47) {
+      allEmojis.forEach(emoji => bot.helpers.deleteEmoji(975295750126846013n, emoji.id!))
+    } else {
+      arr.push(await bot.helpers.createEmoji(975295750126846013n, { name: wantedEmoji, image: `http://ddragon.leagueoflegends.com/cdn/12.13.1/img/champion/${wantedEmoji}.png`}))
+    }
+  } else {
+    arr.push(champEmoji);
+  }
+
+  return arr;
 }
 
 function nFormatter(num: number) {
@@ -133,7 +206,7 @@ function nFormatter(num: number) {
   ];
   
   const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-  var item = lookup.slice().reverse().find(function(item) {
+  const item = lookup.slice().reverse().find(function(item) {
     return num >= item.value;
   });
   return item ? (num / item.value).toFixed(0).replace(rx, "$1") + item.symbol : "0";
--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,6 +1,7 @@
 
 import { ApplicationCommandTypes, ApplicationCommandOptionTypes, Song } from "../../../deps.ts";
 import { reply } from "../../utils/reply.ts"
+import { log } from "../../utils/logger.ts"
 import { createCommand } from "../mod.ts";
 
 createCommand({
@@ -16,54 +17,60 @@ createCommand({
         }
     ],
   execute: async (Bot, interaction) => {
-    if(!interaction.guildId) return reply(Bot, interaction, { content: "Call this command from a guild!" });
+    try {
+        if(!interaction.guildId) return reply(Bot, interaction, { content: "Call this command from a guild!" });
 
-    const userVoiceChannel = Bot.guilds.get(interaction.guildId)?.voiceStates.get(interaction.user.id);
-    if (!userVoiceChannel?.channelId) return reply(Bot, interaction, { content: "You're not currently connected to a voice channel!" });
+        const userVoiceChannel = Bot.guilds.get(interaction.guildId)?.voiceStates.get(interaction.user.id);
+        if (!userVoiceChannel?.channelId) return reply(Bot, interaction, { content: "You're not currently connected to a voice channel!" });
 
-    const song = interaction.data!.options![0].value as string;
+        const song = interaction.data!.options![0].value as string;
 
-    let queue = Bot.lavadeno.queues.get(interaction.guildId);
+        let queue = Bot.lavadeno.queues.get(interaction.guildId);
 
-    const newQueue = queue == undefined;
+        const newQueue = queue == undefined;
 
-    if(!queue) {
-        queue = Bot.lavadeno.createQueue(interaction.guildId!, interaction.channelId!, {
-            songStart: (channelId, song) => {
-                Bot.helpers.sendMessage(channelId, {
-                    content: `Playing ${song.info.title}`,
-                    components: [
-                        {
-                            type: 1,
-                            components: [
-                                {
-                                    type: 2,
-                                    label: "Link",
-                                    style: 5,
-                                    url: song.info.uri,
-                                },
-                            ],
-                        },
-                    ],
-                })
-                .catch(console.error);
-            },
-            finish: (channelId) => {
-                (async () => {
-                    await Bot.helpers.sendMessage(channelId, {content: `Queue finished!`}).catch(console.error);
-                    await queue?.disconnect().destroy();
-                })();
-            },
-        });
+        if(!queue) {
+            queue = Bot.lavadeno.createQueue(interaction.guildId!, interaction.channelId!, {
+                songStart: (channelId, song) => {
+                    Bot.helpers.sendMessage(channelId, {
+                        content: `Playing ${song.info.title}`,
+                        components: [
+                            {
+                                type: 1,
+                                components: [
+                                    {
+                                        type: 2,
+                                        label: "Link",
+                                        style: 5,
+                                        url: song.info.uri,
+                                    },
+                                ],
+                            },
+                        ],
+                    })
+                    .catch(console.error);
+                },
+                finish: (channelId) => {
+                    (async () => {
+                        await Bot.helpers.sendMessage(channelId, {content: `Queue finished!`}).catch(console.error);
+                        await queue?.disconnect().destroy();
+                    })();
+                },
+            });
+        }
+        const tracks = await Bot.lavadeno.core.rest.loadTracks(/*`scsearch:${song}`*/ "ytsearch:" + song);
+
+        if(tracks.tracks.length == 0) return reply(Bot, interaction, { content: "I was unable to find any songs!" });
+        if(!queue.connected) queue.connect(userVoiceChannel.channelId);
+
+        queue.add(new Song(tracks.tracks[0]));
+        await queue.start();
+        
+        await reply(Bot, interaction, { content: newQueue ? "Playing song" : "Song added to queue" });
+    } catch (error) {
+        log.error(error);
+
+        await reply(Bot, interaction, { content: "An unknown error occured, please try again!" });
     }
-    const tracks = await Bot.lavadeno.core.rest.loadTracks(/*`scsearch:${song}`*/ "ytsearch:" + song);
-
-    if(tracks.tracks.length == 0) return reply(Bot, interaction, { content: "I was unable to find any songs!" });
-    if(!queue.connected) queue.connect(userVoiceChannel.channelId);
-
-    queue.add(new Song(tracks.tracks[0]));
-    await queue.start();
-    
-    await reply(Bot, interaction, { content: newQueue ? "Playing song" : "Song added to queue" });
   },
 });
